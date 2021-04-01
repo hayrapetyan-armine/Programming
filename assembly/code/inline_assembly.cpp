a697603de89ac44144856b974108a1c873e790ed
Armine <hayrapet.armine@gmail.com>
2021-04-01 22:19:31 +0400

    Added new code files.

---
+++ assembly/code/inline_assembly.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int arg1, arg2, add, sub, mul ;
+
+    printf( "Enter two integer numbers : " );
+    scanf( "%d%d", &arg1, &arg2 );
+
+    /* Perform Addition, Subtraction, Multiplication */
+    __asm__ ( "addl %%ebx, %%eax;" : "=a" (add) : "a" (arg1) , "b" (arg2) );
+    __asm__ ( "subl %%ebx, %%eax;" : "=a" (sub) : "a" (arg1) , "b" (arg2) );
+    __asm__ ( "imull %%ebx, %%eax;" : "=a" (mul) : "a" (arg1) , "b" (arg2) );
+
+
+    printf( "%d + %d = %d\n", arg1, arg2, add );
+    printf( "%d - %d = %d\n", arg1, arg2, sub );
+    printf( "%d * %d = %d\n", arg1, arg2, mul );
+    return 0;
+}
